ae2e4d18b873c6baa7de4058f4bbae591daaaa4f
Idakronborgn <55064627+Idakronborgn@users.noreply.github.com>
2020-06-09 14:22:49 +0200

    update

--- js/javascriptforside.js
+++ js/javascriptforside.js
@@ -1,11 +1,3 @@
-
-
-
-
-
-function mouseOver() {
-  document.getElementsByClassName(".produktbox").style.color = "red";
-
 function bigImg(x) {
   x.style.height = "290px";
   x.style.width = "180px";
